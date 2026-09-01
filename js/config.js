27231935c7b0a61cdc1042a152d4dfff5fc2e3a1
Guntoro <email-momoservicetebu@gmail.com>
2026-09-01 20:16:16 +0700

    congig

--- js/config.js
+++ js/config.js
@@ -2,6 +2,7 @@
 // FILE: js/config.js
 // ==========================================
 const CONFIG = {
+  SCRIPT_URL: "https://script.google.com/macros/s/AKfycbxx3BLAOh7RZwF2vvukhDPhytbAPXfMP3H_RAJNeWgxLe2LNcCzojm-6HQ1kktPQMTQ/exec",
   GAS_URL: "https://script.google.com/macros/s/AKfycbxx3BLAOh7RZwF2vvukhDPhytbAPXfMP3H_RAJNeWgxLe2LNcCzojm-6HQ1kktPQMTQ/exec",
   MODEL_URL: "https://justadudewhohacks.github.io/face-api.js/models"
 };
